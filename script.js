2f6c7d44c960b649bc57e31a5dc4a888b9a11bae
EvgenPrushk <ogmprushak@mail.ru>
2021-05-31 04:21:00 +0300

    feat: add A

--- script.js
+++ script.js
@@ -1,115 +1,5 @@
-// console.log(
-//   matrixTransformation([
-//     ["#", "#", "#", "#", "#", "#", "#", "#", "#"],
-
-//     ["#", "+", "+", "+", "#", "+", "+", "+", "#"],
-
-//     ["#", "+", "#", "+", "#", "+", "#", "+", "#"],
-
-//     ["+", "+", "#", "+", "0", "+", "#", "+", "#"],
-
-//     ["#", "#", "#", "+", "#", "#", "#", "#", "#"],
-
-//     ["#", "#", "+", "+", "#", "#", "#", "#", "#"],
-
-//     ["#", "#", "+", "#", "#", "#", "#", "#", "#"],
-
-//     ["#", "#", "#", "#", "#", "#", "#", "#", "#"],
-//   ])
-// );
-
-// function matrixTransformation(stringMatrix) {
-//   const matrix = [];
-//   const Start = [];
-//   for (let y = 0; y < stringMatrix.length; y++) {
-//     row = [];
-
-//     for (let x = 0; x < stringMatrix[y].length; x++) {
-//       if (stringMatrix[y][x] === "#") {
-//         row.push(1);
-//       } else if (stringMatrix[y][x] === "+") {
-//         row.push(0);
-//       } else if (stringMatrix[y][x] === "0") {
-//         row.push(0);
-//         Start.push(x, y);
-//       }
-//     }
-
-//     matrix.push(row);
-//   }
-//   const StartPoint = { matrix, Start };
-
-//   return StartPoint;
-// }
-
-// console.log(Potentials([
-// Potentials(
-//   [
-//     ["#", "#", "#", "#", "#", "#", "#", "#", "#"],
-
-//     ["#", "+", "+", "+", "#", "+", "+", "+", "#"],
-
-//     ["#", "+", "#", "+", "#", "+", "#", "+", "#"],
-
-//     ["+", "+", "#", "+", "0", "+", "#", "+", "#"],
-
-//     ["#", "#", "#", "+", "#", "#", "#", "#", "#"],
-
-//     ["#", "#", "+", "+", "#", "#", "#", "#", "#"],
-
-//     ["#", "#", "+", "#", "#", "#", "#", "#", "#"],
-
-//     ["#", "#", "#", "#", "#", "#", "#", "#", "#"],
-//   ],
-
-//   [0, 3]
-// );
-// function Potentials(matrix, [x1, y1], [x2, y2]) {
-//   let potentials = getPotentialMatrix(matrix, [x1, y1], [x2, y2]);
-
-//   console.log(potentials);
-
-//   let potential = potentials[y1][x1];
-
-//   let path = [[x1, y1]];
-
-//   while (potential !== 0) {
-//     // отнимаем от текущего потенциала 1
-//     potential--;
-
-//     // ячейка сверху, равна ли она текущему потенциалу минус 1
-//     if (y1 > 0 && potentials[y1 - 1][x1] === potential) {
-//       path.push([x1, y1 - 1]);
-//       y1--;
-//       continue;
-//     }
-
-//     // ячейка снизу, равна ли она текущему потенциалу минус 1
-//     if (y1 < matrix.length - 1 && potentials[y1 + 1][x1] === potential) {
-//       path.push([x1, y1 + 1]);
-//       y1++;
-//       continue;
-//     }
-
-//     // ячейка слева, равна ли она текущему потенциалу минус 1
-//     if (x1 > 0 && potentials[y1][x1 - 1] === potential) {
-//       path.push([x1 - 1, y1]);
-//       x1--;
-//       continue;
-//     }
-
-//     // ячейка справа, равна ли она текущему потенциалу минус 1
-//     if (x1 < matrix[y].length && potentials[y1][x1 + 1] === potential) {
-//       path.push([x1 + 1, y1]);
-//       x1++;
-//       continue;
-//     }
-//   }
-//   console.log(path);
-// }
-
 console.log(
-  getPotentialMatrix(
+  Potentials(
     [
       ["#", "#", "#", "#", "#", "#", "#", "#", "#"],
 
@@ -125,14 +15,18 @@ console.log(
 
       ["#", "#", "+", "#", "#", "#", "#", "#", "#"],
 
-      ["#", "#", "#", "#", "#", "#", "#", "#", "#"],
+      ["#", "#", "+", "#", "#", "#", "#", "#", "#"],
+      ["#", "#", "+", "#", "#", "#", "#", "#", "#"],
+      ["#", "#", "+", "#", "#", "#", "#", "#", "#"],
+      ["#", "#", "+", "#", "#", "#", "#", "#", "#"],
+      ["#", "#", "+", "+", "#", "#", "#", "#", "#"],
+      ["#", "#", "#", "+", "#", "#", "#", "#", "#"],
     ],
-    [0, 3]
+    [3, 12]
   )
 );
-
-// использую волновую функцию для нохождения выхода из лабиринта
-function getPotentialMatrix(stringMatrix, [x2, y2]) {
+function Potentials(stringMatrix, [x2, y2]) {
+  // Перевод матрицицу в двоичную систему
   const matrix = [];
   const Start = [];
   for (let y = 0; y < stringMatrix.length; y++) {
@@ -151,9 +45,80 @@ function getPotentialMatrix(stringMatrix, [x2, y2]) {
 
     matrix.push(row);
   }
-
-  const potentials = [];
   [x1, y1] = Start;
+
+  potentials = getPotentialMatrix(matrix, [x1, y1], [x2, y2]);
+
+  console.log(potentials);
+
+  let potential = potentials[y1][x1];
+
+  let path = [[x1, y1]];
+
+  while (potential !== 0) {
+    // отнимаем от текущего потенциала 1
+    potential--;
+
+    // ячейка сверху, равна ли она текущему потенциалу минус 1
+    if (y1 > 0 && potentials[y1 - 1][x1] === potential) {
+      path.push([x1, y1 - 1]);
+      y1--;
+      continue;
+    }
+
+    // ячейка снизу, равна ли она текущему потенциалу минус 1
+    if (y1 < matrix.length - 1 && potentials[y1 + 1][x1] === potential) {
+      path.push([x1, y1 + 1]);
+      y1++;
+      continue;
+    }
+
+    // ячейка слева, равна ли она текущему потенциалу минус 1
+    if (x1 > 0 && potentials[y1][x1 - 1] === potential) {
+      path.push([x1 - 1, y1]);
+      x1--;
+      continue;
+    }
+
+    // ячейка справа, равна ли она текущему потенциалу минус 1
+    if (x1 < matrix[y1].length && potentials[y1][x1 + 1] === potential) {
+      path.push([x1 + 1, y1]);
+      x1++;
+      continue;
+    }
+  }
+  
+  const arrayPathString = [];
+
+  for (let i = 1; i < path.length; i++) {
+    if (
+      path[i - 1][1] - path[i][1] === 0 &&
+      path[i - 1][0] - path[i][0] === 1
+    ) {
+      arrayPathString.push("left");
+    } else if (
+      path[i - 1][1] - path[i][1] === 1 &&
+      path[i - 1][0] - path[i][0] === 0
+    ) {
+      arrayPathString.push("top");
+    } else if (
+      path[i - 1][0] - path[i][0] === -1 &&
+      path[i - 1][1] - path[i][1] === 0
+    ) {
+      arrayPathString.push("right");
+    } else {
+      arrayPathString.push("bottom");
+    }
+  }
+  console.log(path);
+
+  return arrayPathString;
+}
+
+// использую волновую функцию для нохождения выхода из лабиринта
+function getPotentialMatrix(matrix, [x1, y1], [x2, y2]) {
+  const potentials = [];
+
   for (let y = 0; y < matrix.length; y++) {
     const row = [];
     for (let x = 0; x < matrix[y].length; x++) {
@@ -206,7 +171,7 @@ function getPotentialMatrix(stringMatrix, [x2, y2]) {
           }
         }
         // правая ячейка
-        if (x < matrix[0].length && potentials[y][x + 1] !== false) {
+        if (x < matrix[0].length - 1 && potentials[y][x + 1] !== false) {
           if (potentials[y][x + 1] === null) {
             potentials[y][x + 1] = number;
           } else {
